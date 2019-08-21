25dcf4357db3f47c4e3ddac13a299e7552315542
小山沙幸 <koyamaisagomiyuki@mbp.local>
2019-08-22 06:09:06 +0900

    five

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module App
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo' 
+    config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
  end
 end
